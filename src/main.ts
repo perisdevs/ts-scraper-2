fa7bf9c40c99ef9edc6e94de96ec376b2ff26172
Peris <perisdevs@gmail.com>
2023-01-29 22:05:40 -0800

    Moved test to a different file, changed test command, configured exports for npm module

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { Scrape as ScrapeExport } from "./Scrape";
+import { Scraper as ScraperExport } from "./Scrape";
+
+export const Scrape = ScrapeExport;
+export const Scraper = ScraperExport;
